44f6343b6c1456d6ab7bb387d7d7841c0fba5d00
Insanelycool <insanelycool1@gmail.com>
2014-07-07 20:21:01 -0400

    Enabled OC up to 2.1GHz & Modified Voltage tables with OC Freqs and Undervolt

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jul 7 20:03:49 EDT 2014"
+#define LINUX_COMPILE_BY "insanelycool"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.31-Insanelycool-Perfomance-OC-UV2-g0fae25e-dirty"
